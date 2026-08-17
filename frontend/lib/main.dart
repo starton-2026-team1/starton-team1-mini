eacca0208f16623ffac1b67a8fa84d3949a8f23e
ncs01060 <ncs01045@gmail.com>
2026-08-17 19:01:19 +0900

    [Design] 앱 실행 시 웰컴 페이지 이동

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'views/welcome_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,6 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    // 앱 실행 시 웰컴 페이지 표시
+    return MaterialApp(home: WelcomePage());
   }
 }
