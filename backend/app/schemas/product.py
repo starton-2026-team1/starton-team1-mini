06ceecdf42dec66ac3cb49b89dbce399f35c64c3
seoyooni <choiseoyooni1016@naver.com>
2026-08-21 16:15:26 +0900

    [Feat] 상품 입력값 검증 추가

--- backend/app/schemas/product.py
+++ backend/app/schemas/product.py
@@ -2,10 +2,11 @@ from pydantic import BaseModel
 
 from app.models.enums import SaleType
 
+from pydantic import BaseModel, Field
 
 class ProductCreate(BaseModel):
-    category_id: int
+    category_id: int = Field(gt=0)
     sale_type: SaleType
-    title: str
-    description: str
-    price: int
+    title: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1)
+    price: int = Field(gt=0)
